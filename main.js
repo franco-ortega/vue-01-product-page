71560f66615b768fa329bc21daed25fbd69409c0
Franco Ortega <francopdx@gmail.com>
2022-05-24 16:22:44 -0700

    add sock image

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        description: 'Warm and fuzzy.'
+        description: 'Warm and fuzzy.',
+        image: './assets/vmSocks-green.png',
+        alt: 'a pair of green socks'
     }
 });
