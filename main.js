7f7bcb41aa11b9cc77664300ea2a66e4d338c6cd
Franco Ortega <francopdx@gmail.com>
2022-05-24 16:04:55 -0700

    add product and description

---
+++ main.js
@@ -0,0 +1,10 @@
+// const product = 'Socks';
+console.log('Hello JS!')
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        description: 'Warm and fuzzy.'
+    }
+});
