848edf6a836433c2591b75e094c7f02d8b7cbe96
Franco Ortega <francopdx@gmail.com>
2022-05-26 16:49:56 -0700

    add computed properties

--- main.js
+++ main.js
@@ -4,12 +4,14 @@ console.log('Hello JS!')
 const app = new Vue({
     el: '#app',
     data: {
+        brand: 'Vue Mastery',
         product: 'Socks',
         description: 'Warm and fuzzy.',
-        image: './assets/vmSocks-green.png',
+        // image: './assets/vmSocks-green.png',
+        selectedVariant: 0,
         alt: 'a pair of green socks',
         sockHref: 'https://www.sockdreams.com/',
-        inStock: true,
+        // inStock: true,
         inventory: 12,
         showMe: false,
         onSale: true,
@@ -18,12 +20,14 @@ const app = new Vue({
             {
                 variantId: 2234,
                 variantColor: 'green',
-                variantImage: './assets/vmSocks-green.png'
+                variantImage: './assets/vmSocks-green.png',
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
-                variantImage: './assets/vmSocks-blue.png'
+                variantImage: './assets/vmSocks-blue.png',
+                variantQuantity: 0
             }
         ],
         sizes: ["XXS", "XS", "S", "M", "L", "XL", "XXL"],
@@ -33,14 +37,29 @@ const app = new Vue({
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            // this.image = variantImage
+            this.selectedVariant = index
         },
         removeFromCart() {
             this.cart -= 1
         },
         soldOut() {
             inStock = false
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        saleMessage() {
+            return this.brand + ' ' + this.product + ' is on sale now!'
         }
     }
 });
